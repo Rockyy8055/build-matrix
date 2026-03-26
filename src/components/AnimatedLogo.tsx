a5cdf6250c7465ece75d97708913bbdd956426d3
Darshan A N <darshanan@Darshans-MacBook-Pro.local>
2026-03-26 21:54:52 +0530

    Add CSS filter to force white color on SVG logo

--- src/components/AnimatedLogo.tsx
+++ src/components/AnimatedLogo.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 export default function AnimatedLogo() {
   return (
-    <motion.div layoutId="main-logo">
+    <motion.div layoutId="main-logo" className="brightness-0 invert">
       <Image
         src="/clients/images/BMlogo.svg"
         alt="Build Matrix"
